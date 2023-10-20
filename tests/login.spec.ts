9bd169d4ae18ed1b452d17798ee2213f61a7ea23
mateuszgrusiecki <grusiecki.mateusz93@gmail.com>
2023-10-20 20:40:07 +0200

    packages

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,27 +3,18 @@ import { test, expect } from '@playwright/test';
 
 test.describe("User login", () => {
 
-//Arrange
-const userLogin = 'gruter08';
-const userPassword = 'resteqwo';
-const expectedUserName = "Jan Demobankowy";
-const tooShortUserLogin = 'gru';
-const tooShortUserNameAlert = "identyfikator ma min. 8 znaków";
-const tooShortPassword = '12345';
-const tooShortPasswordAlert = "hasło ma min. 8 znaków";
-const expectedSignInScreenText = "Wersja demonstracyjna serwisu Demobank";
-
-//Act
 test.beforeEach("open page",async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
+    await page.goto('/');
     await page.waitForLoadState("domcontentloaded") //wait till page will full loaded
-    await page.getByTestId('login-input').click();
 });
 
-
 test('login with correct credentials', async ({ page }) => {
+  //Arrange
+  const userLogin = 'gruter08';
+  const userPassword = 'resteqwo';
+  const expectedUserName = "Jan Demobankowy";
+  //Act
   await page.getByTestId('login-input').fill(userLogin);
-  await page.getByTestId('password-input').click();
   await page.getByTestId('password-input').fill(userPassword);
   await page.getByTestId('login-button').click();
   await page.getByTestId('user-name').click();
@@ -32,21 +23,34 @@ test('login with correct credentials', async ({ page }) => {
 })
 
 test('login with too short login', async ({ page }) => {
+  //Arrange
+  const tooShortUserLogin = 'gru';
+  const tooShortUserNameAlert = "identyfikator ma min. 8 znaków";
+  //Act
   await page.getByTestId('login-input').fill(tooShortUserLogin);
   await page.getByTestId('password-input').click();
   //Assert
   await expect (page.getByTestId('error-login-id')).toHaveText(tooShortUserNameAlert);
 })
 test('login with too short password', async ({ page }) => {
-
+  //Arrange
+  const userLogin = 'gruter08';
+  const tooShortPassword = '12345';
+  const tooShortPasswordAlert = "hasło ma min. 8 znaków";
+  //Act
   await page.getByTestId('login-input').fill(userLogin);
-  await page.getByTestId('password-input').click();
   await page.getByTestId('password-input').fill(tooShortPassword);
   await page.getByTestId('login-button').click({ force: true });
   //Assert
   await expect (page.getByTestId('error-login-password')).toHaveText(tooShortPasswordAlert);
 })
 test('correct login and logout', async ({ page }) =>{
+    //Arrange
+    const userLogin = 'gruter08';
+    const userPassword = 'resteqwo';
+    const expectedUserName = "Jan Demobankowy";
+    const expectedSignInScreenText = "Wersja demonstracyjna serwisu Demobank";
+    //Act
     await page.getByTestId('login-input').fill(userLogin);
     await page.getByTestId('password-input').click();
     await page.getByTestId('password-input').fill(userPassword);
