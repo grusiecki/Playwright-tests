47362ebdae45c966172f4ace68cad298d0bf0e45
mateuszgrusiecki <grusiecki.mateusz93@gmail.com>
2023-10-24 19:26:47 +0200

    addPages

--- tests/Panel.spec.ts
+++ tests/Panel.spec.ts
@@ -1,34 +1,36 @@
 import { test, expect } from "@playwright/test";
 import { loginData } from "../test-data/login.data";
+import { LoginPage } from "../pages/login.page";
+import { PanelPage } from "../pages/panel.page";
 
 test.describe("Panel operations", () => {
+  let loginPage: LoginPage;
+  let panelPage: PanelPage;
+  const userLogin = loginData.userId;
+  const userPassword = loginData.password;
+  
+  test.beforeEach("open page", async ({ page }) => {
+    await page.goto('/');
+    await page.waitForLoadState("domcontentloaded") //wait till page will full loaded
+    loginPage = new LoginPage(page);
+    panelPage = new PanelPage(page);
+    await loginPage.login(userLogin, userPassword); 
+    await page.waitForLoadState("domcontentloaded") //wait till page will full loaded
+
+  });
+  test('Money transfer', async ({ page }) => {
+    const moneyReceiver = "2";
+    const moneyAmount = "1004";
+    const transferTitle = "Thank you";
+    const confirmationText = `Przelew wykonany! Chuck Demobankowy - ${moneyAmount},00PLN - ${transferTitle}`;
     
-    const userLogin = loginData.userId;
-    const userPassword = loginData.password;
-    
+    await panelPage.widgetTransferReceiver.selectOption(moneyReceiver);
+    await panelPage.transferAmountInput.fill(moneyAmount);
+    await panelPage.transferTitleInput.fill(transferTitle);
+    await panelPage.sendMoneyButton.click();
+    await page.waitForLoadState("domcontentloaded");
+    await panelPage.transferConfirmationPopUp.click();
 
-
-    test.beforeEach("open page", async ({ page }) => {
-        await page.goto("/");
-        await page.waitForLoadState("domcontentloaded"); //wait till page will full loaded
-        await page.getByTestId("login-input").fill(userLogin);
-        await page.getByTestId("password-input").fill(userPassword);
-        await page.getByTestId("login-button").click();
-        await page.getByTestId("user-name").click();
-      });
-      test('Money transfer', async ({ page }) => {
-        const moneyReceiver = "2";
-        const moneyAmount = "1004";
-        const transferTitle = "Thank you";
-        const confirmationText = `Przelew wykonany! Chuck Demobankowy - ${moneyAmount},00PLN - ${transferTitle}`;
-
-      await page.locator('#widget_1_transfer_receiver').selectOption(moneyReceiver);
-      await page.locator('#widget_1_transfer_amount').fill(moneyAmount);
-      await page.locator('#widget_1_transfer_title').fill(transferTitle);
-      await page.getByRole('button', { name: 'wykonaj' }).click();
-      await page.waitForLoadState("domcontentloaded");
-      await page.getByTestId('close-button').click();
-      
-      await expect (page.getByRole('link', { name: confirmationText })).toHaveText(confirmationText);
-      });
+    await expect(page.getByRole('link', { name: confirmationText })).toHaveText(confirmationText);
+  });
 });
