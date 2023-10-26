8230d872f90695361e31f62d9c16e94479b37ac1
mateuszgrusiecki <grusiecki.mateusz93@gmail.com>
2023-10-26 14:43:54 +0200

    adding topUp test

--- pages/panel.page.ts
+++ pages/panel.page.ts
@@ -1,11 +1,26 @@
 import { Page } from '@playwright/test'
 
 export class PanelPage {
-    constructor(private page: Page) {}
-widgetTransferReceiver = this.page.locator('#widget_1_transfer_receiver');
-transferAmountInput = this.page.locator('#widget_1_transfer_amount');
-transferTitleInput = this.page.locator('#widget_1_transfer_title');
-sendMoneyButton = this.page.getByRole('button', { name: 'wykonaj' });
-transferConfirmationPopUp = this.page.getByTestId('close-button');
+    constructor(private page: Page) { }
+    widgetTransferReceiver = this.page.locator('#widget_1_transfer_receiver');
+    transferAmountInput = this.page.locator('#widget_1_transfer_amount');
+    transferTitleInput = this.page.locator('#widget_1_transfer_title');
+    sendMoneyButton = this.page.getByRole('button', { name: 'wykonaj' });
+    transferConfirmationPopUp = this.page.getByTestId('close-button');
+
+    topUpReceiver = this.page.locator('#widget_1_topup_receiver');
+    topUpAmount = this.page.locator('#widget_1_topup_amount');
+    termsTopUpCheckbox = this.page.locator('#uniform-widget_1_topup_agreement span');
+    topUpConfirmationButton = this.page.getByRole('button', { name: 'doładuj telefon' });
+
+    async topUp(topUpReceiver: string, topUpAmount: string): Promise<void> {
+        
+        await this.topUpReceiver.click();
+        await this.topUpReceiver.selectOption(topUpReceiver);
+        await this.topUpAmount.fill(topUpAmount);
+        await this.termsTopUpCheckbox.click();
+        await this.topUpConfirmationButton.click();
+        await this.transferConfirmationPopUp.click();
+    }
 
 }
--- tests/Panel.spec.ts
+++ tests/Panel.spec.ts
@@ -1,10 +1,14 @@
 import { test, expect } from "@playwright/test";
 import { loginData } from "../test-data/login.data";
+import { PanelPage } from "../pages/panel.page";
+import { LoginPage } from "../pages/login.page";
 
 test.describe("Panel operations", () => {
     
     const userLogin = loginData.userId;
     const userPassword = loginData.password;
+    let loginPage: LoginPage;
+    let panelPage: PanelPage;
     
 
 
@@ -29,6 +33,17 @@ test.describe("Panel operations", () => {
       await page.waitForLoadState("domcontentloaded");
       await page.getByTestId('close-button').click();
       
+      await expect (page.getByRole('link', { name: confirmationText })).toHaveText(confirmationText);
+      });
+      test('topUp', async ({ page }) => {
+        const topUpReceiver = '500 xxx xxx';
+        const topUpAmount = "30";
+        const confirmationText = 'Doładowanie wykonane! 30,00PLN na numer 500 xxx xxx';
+        panelPage = new PanelPage(page);
+        
+        await page.waitForLoadState("domcontentloaded") //wait till page will full loaded
+        await panelPage.topUp(topUpReceiver, topUpAmount);
+
       await expect (page.getByRole('link', { name: confirmationText })).toHaveText(confirmationText);
       });
 });
